4f878c7dbfe012d9d0de0e5254ea3a65be8fa278
Vlad Istodorescu <166702009+vladistodorescu@users.noreply.github.com>
2025-11-06 13:54:34 +0200

    Create WorkingHoursController.java

---
+++ WorkingHoursController.java
@@ -0,0 +1,7 @@
+// the file's name does not have ".java" extension, but APEX classes are very similar to Java code
+
+public with sharing class WorkingHoursController {
+    public WorkingHoursController() {
+
+    }
+}
