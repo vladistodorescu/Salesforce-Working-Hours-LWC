5c46227947ad9bfc31b376a157fd0741429a85ea
Vlad Istodorescu <166702009+vladistodorescu@users.noreply.github.com>
2025-11-12 15:22:21 +0200

    workingHours.js v4 (full functionality)

--- workingHours.js
+++ workingHours.js
@@ -1,14 +1,12 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { refreshApex } from '@salesforce/apex';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 
 import WORKING_DAYS_OBJECT from '@salesforce/schema/Working_Day__c';
 import HOURS_PICKLIST_FIELD from '@salesforce/schema/Working_Day__c.Start_Hour__c';
-import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-import storeSchedule from '@salesforce/apex/WorkingHoursController.storeSchedule';
 
-// make the LWC throw error if i'm trying to save second interval without first interval
+import storeSchedule from '@salesforce/apex/WorkingHoursController.storeSchedule';
 
 const intervalKeys = [
     'mondayStartFirstInterval','mondayEndFirstInterval',
@@ -28,6 +26,12 @@ export default class WorkingHours extends LightningElement {
     showSchedulerBool = false;
 
     recordTypeId;
+
+    mondaySelected = false;
+    tuesdaySelected = false;
+    wednesdaySelected = false;
+    thursdaySelected = false;
+    fridaySelected = false;
 
     // remember to later verify later before saving, that the start time is before the end time
     mondayStartFirstInterval = null;
@@ -72,128 +76,7 @@ export default class WorkingHours extends LightningElement {
         this.showSchedulerBool = true;
     }
 
-    // runs in current user's context, gets the right record type id for the Account object
-    @wire(getObjectInfo, {objectApiName: WORKING_DAYS_OBJECT})
-    wiredRecordType({data, error}) {
-        if (data) {
-            this.recordTypeId = data.defaultRecordTypeId;
-            console.log('recordTypeId: ' + this.recordTypeId);
-        } else if (error) {
-            console.log('Error when retrieving the recordTypeId for the running user: ' + error);
-        }
-    }
-
-    // retrieve the picklist values and store them
-    @wire(getPicklistValues, {recordTypeId: '$recordTypeId', fieldApiName: HOURS_PICKLIST_FIELD})
-    wiredPicklistOptions({data, error}) {
-        if (data) {
-            this.hoursPicklistOptions = data.values;
-            console.log('hoursPicklistOptions: ' + this.hoursPicklistOptions);
-        } else if (error) {
-            console.log('Error when retrieving the picklist values for the hours field: ' + error);
-        }
-    }
-
-    // getter for combobox values, create a label-value pair array for the combobox
-    get hourOptions(){
-        if (!this.hoursPicklistOptions){
-            return [];
-        }
-
-        const opts = []; 
-        for (let i = 0; i < this.hoursPicklistOptions.length; i++){
-            opts.push({
-                label: this.hoursPicklistOptions[i].label,
-                value: this.hoursPicklistOptions[i].value
-            });
-        }
-        return opts;   
-    }
-
-    // Handlers for First Interval
-
-    handleMondayStartFirstIntervalChange(event) {
-        this.mondayStartFirstInterval = event.detail.value;
-    }
-
-    handleMondayEndFirstIntervalChange(event){
-        this.mondayEndFirstInterval = event.detail.value;
-    }
-
-    handleTuesdayStartFirstIntervalChange(event) {
-        this.tuesdayStartFirstInterval = event.detail.value;
-    }
-
-    handleTuesdayEndFirstIntervalChange(event){
-        this.tuesdayEndFirstInterval = event.detail.value;
-    } 
-
-    handleWednesdayStartFirstIntervalChange(event){
-        this.wednesdayStartFirstInterval = event.detail.value;
-    }
-
-    handleWednesdayEndFirstIntervalChange(event){
-        this.wednesdayEndFirstInterval = event.detail.value;
-    }
-
-    handleThursdayStartFirstIntervalChange(event){
-        this.thursdayStartFirstInterval = event.detail.value;
-    }
-
-    handleThursdayEndFirstIntervalChange(event){
-        this.thursdayEndFirstInterval = event.detail.value;
-    }
-
-    handleFridayStartFirstIntervalChange(event){
-        this.fridayStartFirstInterval = event.detail.value;
-    }
-
-    handleFridayEndFirstIntervalChange(event){
-        this.fridayEndFirstInterval = event.detail.value;
-    }
-
-    // Handlers for Second Interval
-
-    handleMondayStartSecondIntervalChange(event) {
-        this.mondayStartSecondInterval = event.detail.value;
-    }
-
-    handleMondayEndSecondIntervalChange(event){
-        this.mondayEndSecondInterval = event.detail.value;
-    }
-
-    handleTuesdayStartSecondIntervalChange(event) {
-        this.tuesdayStartSecondInterval = event.detail.value;
-    }
-
-    handleTuesdayEndSecondIntervalChange(event){
-        this.tuesdayEndSecondInterval = event.detail.value;
-    } 
-
-    handleWednesdayStartSecondIntervalChange(event){
-        this.wednesdayStartSecondInterval = event.detail.value;
-    }
-
-    handleWednesdayEndSecondIntervalChange(event){
-        this.wednesdayEndSecondInterval = event.detail.value;
-    }
-
-    handleThursdayStartSecondIntervalChange(event){
-        this.thursdayStartSecondInterval = event.detail.value;
-    }
-
-    handleThursdayEndSecondIntervalChange(event){
-        this.thursdayEndSecondInterval = event.detail.value;
-    }
-
-    handleFridayStartSecondIntervalChange(event){
-        this.fridayStartSecondInterval = event.detail.value;
-    }
-
-    handleFridayEndSecondIntervalChange(event){
-        this.fridayEndSecondInterval = event.detail.value;
-    }
-
+    // maybe also add spinner while saving
     async saveSchedule(){
         if (this.isSaving){
             return;
@@ -252,45 +135,149 @@ export default class WorkingHours extends LightningElement {
         this.thursdayExtraCombobox = false;
         this.fridayExtraCombobox = false;
     }
-     
-    addMondayExtraCombobox(){
-        this.mondayExtraCombobox = true;
+
+    // runs in current user's context, gets the right record type id for the Working_Days__c object
+    @wire(getObjectInfo, {objectApiName: WORKING_DAYS_OBJECT})
+    wiredRecordType({data, error}) {
+        if (data) {
+            this.recordTypeId = data.defaultRecordTypeId;
+            console.log('recordTypeId: ' + this.recordTypeId);
+        } else if (error) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error retrieving running user\'s recordTypeId',
+                message: 'Error retrieving running user\'s recordTypeId!',
+                variant: 'error'
+            }));
+        }
     }
 
-    addTuesdayExtraCombobox(){
-        this.tuesdayExtraCombobox = true;
+    // retrieve the picklist values and store them
+    @wire(getPicklistValues, {recordTypeId: '$recordTypeId', fieldApiName: HOURS_PICKLIST_FIELD})
+    wiredPicklistOptions({data, error}) {
+        if (data) {
+            this.hoursPicklistOptions = data.values;
+            console.log('hoursPicklistOptions: ' + this.hoursPicklistOptions);
+        } else if (error) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error retrieving running user\'s picklist fields',
+                message: 'Error retrieving running user\'s picklist fields!',
+                variant: 'error'
+            }));
+        }
     }
 
-    addWednesdayExtraCombobox(){
-        this.wednesdayExtraCombobox = true;
+    // getter for combobox values, create a label-value pair array for the combobox
+    get hourOptions(){
+        if (!this.hoursPicklistOptions){
+            return [];
+        }
+
+        const opts = []; 
+        for (let i = 0; i < this.hoursPicklistOptions.length; i++){
+            opts.push({
+                label: this.hoursPicklistOptions[i].label,
+                value: this.hoursPicklistOptions[i].value
+            });
+        }
+        return opts;   
     }
 
-    addThursdayExtraCombobox(){
-        this.thursdayExtraCombobox = true;
-    }
-    
-    addFridayExtraCombobox(){
-        this.fridayExtraCombobox = true;
-    }
+    // class helpers (blue only when selected)
+    get sunClass() { return 'day-chip'; }
+    get satClass() { return 'day-chip'; }
 
-    removeMondayExtraCombobox(){
-        this.mondayExtraCombobox = false;
-    }
+    get monClass() { return `day-chip ${this.mondaySelected ? 'day-chip_on' : ''}`; }
+    get tueClass() { return `day-chip ${this.tuesdaySelected ? 'day-chip_on' : ''}`; }
+    get wedClass() { return `day-chip ${this.wednesdaySelected ? 'day-chip_on' : ''}`; }
+    get thuClass() { return `day-chip ${this.thursdaySelected ? 'day-chip_on' : ''}`; }
+    get friClass() { return `day-chip ${this.fridaySelected ? 'day-chip_on' : ''}`; }
 
-    removeTuesdayExtraCombobox(){
-        this.tuesdayExtraCombobox = false;
-    }
+    // Handlers for First Interval
+    handleMondayStartFirstIntervalChange(event) { this.mondayStartFirstInterval = event.detail.value; }
+    handleMondayEndFirstIntervalChange(event){ this.mondayEndFirstInterval = event.detail.value; }
+    handleTuesdayStartFirstIntervalChange(event) { this.tuesdayStartFirstInterval = event.detail.value; }
+    handleTuesdayEndFirstIntervalChange(event){ this.tuesdayEndFirstInterval = event.detail.value; } 
+    handleWednesdayStartFirstIntervalChange(event){ this.wednesdayStartFirstInterval = event.detail.value; }
+    handleWednesdayEndFirstIntervalChange(event){ this.wednesdayEndFirstInterval = event.detail.value; }
+    handleThursdayStartFirstIntervalChange(event){ this.thursdayStartFirstInterval = event.detail.value; }
+    handleThursdayEndFirstIntervalChange(event){ this.thursdayEndFirstInterval = event.detail.value; }
+    handleFridayStartFirstIntervalChange(event){ this.fridayStartFirstInterval = event.detail.value; }
+    handleFridayEndFirstIntervalChange(event){ this.fridayEndFirstInterval = event.detail.value; }
 
-    removeWednesdayExtraCombobox(){
-        this.wednesdayExtraCombobox = false;
-    }
+    // Handlers for Second Interval
+    handleMondayStartSecondIntervalChange(event) { this.mondayStartSecondInterval = event.detail.value; }
+    handleMondayEndSecondIntervalChange(event){ this.mondayEndSecondInterval = event.detail.value; }
+    handleTuesdayStartSecondIntervalChange(event) { this.tuesdayStartSecondInterval = event.detail.value; }
+    handleTuesdayEndSecondIntervalChange(event){ this.tuesdayEndSecondInterval = event.detail.value;} 
+    handleWednesdayStartSecondIntervalChange(event){ this.wednesdayStartSecondInterval = event.detail.value; }
+    handleWednesdayEndSecondIntervalChange(event){ this.wednesdayEndSecondInterval = event.detail.value; }
+    handleThursdayStartSecondIntervalChange(event){ this.thursdayStartSecondInterval = event.detail.value; }
+    handleThursdayEndSecondIntervalChange(event){ this.thursdayEndSecondInterval = event.detail.value; }
+    handleFridayStartSecondIntervalChange(event){ this.fridayStartSecondInterval = event.detail.value; }
+    handleFridayEndSecondIntervalChange(event){ this.fridayEndSecondInterval = event.detail.value; }
 
-    removeThursdayExtraCombobox(){
-        this.thursdayExtraCombobox = false;
-    }
-    
-    removeFridayExtraCombobox(){
-        this.fridayExtraCombobox = false;
-    }
+    // Methods for adding and removing extra comboboxes
+    addMondayExtraCombobox(){ this.mondayExtraCombobox = true; }
+    addTuesdayExtraCombobox(){ this.tuesdayExtraCombobox = true; }
+    addWednesdayExtraCombobox(){ this.wednesdayExtraCombobox = true; }
+    addThursdayExtraCombobox(){ this.thursdayExtraCombobox = true; }
+    addFridayExtraCombobox(){ this.fridayExtraCombobox = true; }
 
+    removeMondayExtraCombobox(){ this.mondayExtraCombobox = false; }
+    removeTuesdayExtraCombobox(){ this.tuesdayExtraCombobox = false; }
+    removeWednesdayExtraCombobox(){ this.wednesdayExtraCombobox = false; }
+    removeThursdayExtraCombobox(){ this.thursdayExtraCombobox = false; }
+    removeFridayExtraCombobox(){ this.fridayExtraCombobox = false; }
+
+    // Methods for enabling day for scheduling
+    toggleMonday(){ 
+        this.mondaySelected = !this.mondaySelected;
+        if (this.mondaySelected == false){
+            this.mondayStartFirstInterval = null;
+            this.mondayEndFirstInterval = null;
+            this.mondayStartSecondInterval = null;
+            this.mondayEndSecondInterval = null;
+            this.mondayExtraCombobox = false;
+        }
+    }
+    toggleTuesday(){ 
+        this.tuesdaySelected = !this.tuesdaySelected;
+        if (this.tuesdaySelected == false){
+            this.tuesdayStartFirstInterval = null;
+            this.tuesdayEndFirstInterval = null;
+            this.tuesdayStartSecondInterval = null;
+            this.tuesdayEndSecondInterval = null;
+            this.tuesdayExtraCombobox = false;
+        }
+    }
+    toggleWednesday(){ 
+        this.wednesdaySelected = !this.wednesdaySelected;
+        if (this.wednesdaySelected == false){
+            this.wednesdayStartFirstInterval = null;
+            this.wednesdayEndFirstInterval = null;
+            this.wednesdayStartSecondInterval = null;
+            this.wednesdayEndSecondInterval = null;
+            this.wednesdayExtraCombobox = false;
+        }
+    }
+    toggleThursday() { 
+        this.thursdaySelected = !this.thursdaySelected;
+        if (thursdaySelected == false){
+            this.thursdayStartFirstInterval = null;
+            this.thursdayEndFirstInterval = null;
+            this.thursdayStartSecondInterval = null;
+            this.thursdayEndSecondInterval = null;
+            this.thursdayExtraCombobox = false;
+        }
+    }
+    toggleFriday(){ 
+        this.fridaySelected = !this.fridaySelected;
+        if (this.fridaySelected == false){
+            this.fridayStartFirstInterval = null;
+            this.fridayEndFirstInterval = null;
+            this.fridayStartSecondInterval = null;
+            this.fridayEndSecondInterval = null;
+            this.fridayExtraCombobox = false;
+        }
+    }
 }
